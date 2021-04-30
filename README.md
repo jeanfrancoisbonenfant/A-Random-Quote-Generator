0396358a090a52b7e420426c8e34d88b34864f3f
Jean-Francois Bonenfant <up4aparty_24_7@hotmail.com>
2021-04-30 18:33:38 -0400

    Text format update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # A Random Quote Generator
- Unit 1 Project for Treehouse Full stack Javascript techdegree
 
- A random Quote Generator
+Unit 1 Project for Treehouse Full stack Javascript techdegree
 
- Display a random Quote every time the user click the Show another Quote & change Quote automatically every X seconds.
- The background color change with the quote as well as the Quote box and the Button background color.
+A random Quote Generator
+
+Display a random Quote every time the user click the Show another Quote & change Quote automatically every X seconds.
+The background color change with the quote as well as the Quote box and the Button background color.
--- js/script.js
+++ js/script.js
@@ -1,37 +1,44 @@
-// Array of object containing Quotes 
+// Array of object containing Quotes
 const quotes = [
   {
-    quote: "Once you have tasted flight, you will forever walk the earth with your eyes turned skywards.",
+    quote:
+      "Once you have tasted flight, you will forever walk the earth with your eyes turned skywards.",
     source: "Leonardo Da Vinci",
     image: "./image/Leonardo.jpeg",
   },
   {
-    quote: "When everything seems to be going against you, remeber that aircraft take off against the wind, not with it",
+    quote:
+      "When everything seems to be going against you, remeber that aircraft take off against the wind, not with it",
     source: "Henry Ford",
     image: "./image/Henry ford.jpeg",
   },
   {
-    quote: "The future belongs to those who believe in the beauty of their dreams",
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams",
     source: "Eleanor Roosevelt",
     image: "./image/Roosevelt_Eleanor.jpeg",
   },
   {
-    quote: "Too many people have opinions on things they know nothing about. And the more ignorant they are, the more opinions they have.",
+    quote:
+      "Too many people have opinions on things they know nothing about. And the more ignorant they are, the more opinions they have.",
     source: "Thomas Hildern",
     citation: "Fallout: New Vegas",
-    year: 2009
+    year: 2009,
   },
   {
-    quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
     source: "Albert Einstein",
     image: "./image/Albert.jpeg",
   },
   {
-    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    quote:
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
     source: "Benjamin Franklin",
   },
   {
-    quote: "It is better to be hated for what you are than to be loved for what you are not.",
+    quote:
+      "It is better to be hated for what you are than to be loved for what you are not.",
     source: "Andre Gide",
     citation: "Autumn Leaves",
     year: 1950,
@@ -40,8 +47,8 @@ const quotes = [
 
 //Function getRandomQuote pick a random quote from the Array then return it.
 const getRandomQuote = () => {
-    let randomNumber = Math.floor(Math.random() * quotes.length)
-      return quotes[randomNumber];
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
 };
 
 /* Function printQuote utilize the randomQuote selected 
@@ -49,61 +56,60 @@ by the previous getRandomQuote function & create a layout
 to be presented throught the message variable. 
 */
 const printQuote = () => {
-  let nextQuote = getRandomQuote(quotes);
+  const nextQuote = getRandomQuote(quotes);
   let message = `<p class = "quote"> ${nextQuote.quote} </p>
                 <p class = "source"> ${nextQuote.source}`;
-                  if (nextQuote.citation !== undefined){
-                  message += `<span class="citation">${nextQuote.citation}</span>`;
-                  }
-                  if (nextQuote.year !== undefined){
-                    message += `<span class="year">${nextQuote.year}</span>`;
-                  }
-                  if (nextQuote.image !== undefined){
-                    message += `<img src="${nextQuote.image}"alt="image of ${nextQuote.source}">`;
-                  }
-                  message += `</p>`; 
+  if (nextQuote.citation !== undefined) {
+    message += `<span class="citation">${nextQuote.citation}</span>`;
+  }
+  if (nextQuote.year !== undefined) {
+    message += `<span class="year">${nextQuote.year}</span>`;
+  }
+  if (nextQuote.image !== undefined) {
+    message += `<img src="${nextQuote.image}"alt="image of ${nextQuote.source}">`;
+  }
+  message += `</p>`;
 
-//The layout created earlier is sent to the innerHTML of the quote-box.                    
-document.getElementById('quote-box').innerHTML = message;                    
- 
+  //The layout created earlier is sent to the innerHTML of the quote-box.
+  document.getElementById("quote-box").innerHTML = message;
 
-/*Function colorGenerator simply create a random number on 256base
+  /*Function colorGenerator simply create a random number on 256base
  to be pass inside randomRGB to create a random RGB color.*/
-const colorGenerator = () => Math.floor(Math.random() * 256);
+  const colorGenerator = () => Math.floor(Math.random() * 256);
 
-
-/*Function randomRGB take the random number created by colorGenerator
+  /*Function randomRGB take the random number created by colorGenerator
 to form a variable to hold a RGB color layout RGB(RED,GREEN,BLUE,Opacity)
 */
-const randomRGB = (value) => {
-const color = `rgb(${value()}, ${value()}, ${value()}, 0.2)`
-  return color;
-};
+  const randomRGB = (value) => {
+    const color = `rgb(${value()}, ${value()}, ${value()}, 0.2)`;
+    return color;
+  };
 
-//The random RGB color generated by randomRGB is set to style the backgroundColor of the load-quote ID
-document.getElementById('load-quote').style.backgroundColor = randomRGB(colorGenerator);
+  //The random RGB color generated by randomRGB is set to style the backgroundColor of the load-quote ID
+  document.getElementById("load-quote").style.backgroundColor = randomRGB(
+    colorGenerator
+  );
 
-//The random RGB color generated by randomRGB is set to style the backgroundColor of the container ID
-document.getElementById('container').style.backgroundColor = randomRGB(colorGenerator); 
+  //The random RGB color generated by randomRGB is set to style the backgroundColor of the container ID
+  document.getElementById("container").style.backgroundColor = randomRGB(
+    colorGenerator
+  );
 
+  //Addtionnal option to get a random color for the body background as well.
+  document.body.style = `background-color: ${randomRGB(colorGenerator)}`;
 
- //Addtionnal option to get a random color for the body background as well.
-document.body.style = `background-color: ${randomRGB(colorGenerator)}`;
-
-
-
-//Function image replace the body background by a random image 1680x1050.
-/*
+  //Function image replace the body background by a random image 1680x1050.
+  /*
 const image = () => {
   document.body.style.backgroundImage = "url(https://picsum.photos/1680/1050?random=2)"; 
 };
 
 /*function image is called with the PrintQuote function
  to change the background image everytime PrintQuote function is called.*/
- /*
+  /*
 image();   
 */
-//closing bracket of printQuote Function
+  //closing bracket of printQuote Function
 };
 
 //set a timer to call the printQuote function every 5sec to change the Quote displayed.
@@ -112,5 +118,7 @@ setInterval(printQuote, 10000);
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+ ***/
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
